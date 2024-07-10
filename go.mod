316ef4d6160bd2f1c5ba75a72b578d4396b66ab6
Niranjan Dahal <niranjandahal76@gmail.com>
2024-07-10 21:24:14 +0545

    image resizer

--- go.mod
+++ go.mod
@@ -1,15 +1,5 @@
-module hostedgo
+module imageresizer
 
-go 1.18
+go 1.22.3
 
-require (
-	github.com/gorilla/mux v1.8.0
-	gorm.io/driver/sqlite v1.1.5
-	gorm.io/gorm v1.23.5
-)
-
-require (
-	github.com/jinzhu/inflection v1.0.0 // indirect
-	github.com/jinzhu/now v1.1.4 // indirect
-	github.com/mattn/go-sqlite3 v1.14.8 // indirect
-)
+require github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646
--- main.go
+++ main.go
@@ -1,28 +1,124 @@
 package main
 
 import (
-    "log"
-    "net/http"
-    "hostedgo/database"
-    "hostedgo/handlers"
+	"fmt"
+	"html/template"
+	"image"
+	"image/jpeg"
+	_ "image/png"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strconv"
+	"time"
 
-    "github.com/gorilla/mux"
+	"github.com/nfnt/resize"
 )
 
+func uploadHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.ServeFile(w, r, "static/upload.html")
+		return
+	}
+
+	file, _, err := r.FormFile("image")
+	if err != nil {
+		http.Error(w, "Failed to upload image", http.StatusInternalServerError)
+		return
+	}
+	defer file.Close()
+
+	img, _, err := image.Decode(file)
+	if err != nil {
+		http.Error(w, "Failed to decode image", http.StatusInternalServerError)
+		return
+	}
+
+	width, err := strconv.Atoi(r.FormValue("width"))
+	if err != nil {
+		http.Error(w, "Invalid width", http.StatusBadRequest)
+		return
+	}
+
+	height, err := strconv.Atoi(r.FormValue("height"))
+	if err != nil {
+		http.Error(w, "Invalid height", http.StatusBadRequest)
+		return
+	}
+
+	resizedImg := resize.Resize(uint(width), uint(height), img, resize.Lanczos3)
+
+	fileName := fmt.Sprintf("resized_%d.jpg", time.Now().UnixNano())
+	filePath := filepath.Join("static", "uploads", fileName)
+	os.MkdirAll(filepath.Dir(filePath), os.ModePerm)
+
+	outFile, err := os.Create(filePath)
+	if err != nil {
+		http.Error(w, "Failed to create file", http.StatusInternalServerError)
+		return
+	}
+	defer outFile.Close()
+
+	err = jpeg.Encode(outFile, resizedImg, nil)
+	if err != nil {
+		http.Error(w, "Failed to encode image", http.StatusInternalServerError)
+		return
+	}
+
+	data := map[string]string{
+		"FilePath": "/download/" + fileName,
+	}
+
+	tmpl, err := template.ParseFiles("static/download.html")
+	if err != nil {
+		http.Error(w, "Failed to load template", http.StatusInternalServerError)
+		return
+	}
+	err = tmpl.Execute(w, data)
+	if err != nil {
+		http.Error(w, "Failed to render template", http.StatusInternalServerError)
+		return
+	}
+}
+
+func downloadHandler(w http.ResponseWriter, r *http.Request) {
+	fileName := r.URL.Path[len("/download/"):]
+	filePath := filepath.Join("static", "uploads", fileName)
+
+	file, err := os.Open(filePath)
+	if err != nil {
+		http.Error(w, "Failed to open file", http.StatusInternalServerError)
+		return
+	}
+	defer file.Close()
+
+	w.Header().Set("Content-Disposition", "attachment; filename="+fileName)
+	w.Header().Set("Content-Type", "image/jpeg")
+
+	http.ServeContent(w, r, fileName, time.Now(), file)
+}
+
+func notFoundHandler(w http.ResponseWriter, _ *http.Request) {
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprint(w, "404 - Page not found")
+}
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		notFoundHandler(w, r)
+		return
+	}
+	http.ServeFile(w, r, "static/upload.html")
+}
+
 func main() {
-    var err error
-    database.DB, err = database.InitDB()
-    if err != nil {
-        log.Fatal(err)
-    }
+	http.HandleFunc("/", rootHandler)
+	http.HandleFunc("/upload", uploadHandler)
+	http.HandleFunc("/download/", downloadHandler)
 
-    router := mux.NewRouter()
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 
-    router.HandleFunc("/items", handlers.CreateItem).Methods("POST")
-    router.HandleFunc("/items", handlers.GetItems).Methods("GET")
-    router.HandleFunc("/items/{id}", handlers.GetItem).Methods("GET")
-    router.HandleFunc("/items/{id}", handlers.UpdateItem).Methods("PUT")
-    router.HandleFunc("/items/{id}", handlers.DeleteItem).Methods("DELETE")
-
-    log.Fatal(http.ListenAndServe(":8000", router))
+	fmt.Println("Server started at :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
